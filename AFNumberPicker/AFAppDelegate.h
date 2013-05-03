4e5595a51998c2ff8d7d172a25210800a3bb7eb0
Philip Vasilchenko <philip.vasilchenko@gmail.com>
2013-05-03 19:50:50 +0300

    Initial Commit

---
+++ AFNumberPicker/AFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AFAppDelegate.h
+//  AFNumberPicker
+//
+//  Created by Philip Vasilchenko on 03.05.13.
+//  Copyright (c) 2013 ArtFeel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AFNumberPicker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AFNumberPicker
+//
+//  Created by Philip Vasilchenko on 03.05.13.
+//  Copyright (c) 2013 ArtFeel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AFAppDelegate class]));
+    }
+}
