f7eaea4bffaf2ad5bc99d43399be20809067e776
Philip Vasilchenko <philip.vasilchenko@gmail.com>
2013-05-03 20:35:00 +0300

    Post initial commit

---
+++ AFNumberPicker/AFNumberPickerExample/Classes/AFAppDelegate.m
@@ -0,0 +1,24 @@
+//
+//  AFAppDelegate.m
+//  AFNumberPicker
+//
+//  Created by Philip Vasilchenko on 03.05.13.
+//  Copyright (c) 2013 ArtFeel. All rights reserved.
+//
+
+#import "AFAppDelegate.h"
+#import "AFNumberPickerExapmleViewController.h"
+
+
+@implementation AFAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+
+    [self.window setRootViewController:[AFNumberPickerExapmleViewController new]];
+    [self.window makeKeyAndVisible];
+
+    return YES;
+}
+
+@end
---
+++ AFNumberPicker/AFNumberPickerExample/Classes/AFNumberPickerExapmleViewController.m
@@ -0,0 +1,27 @@
+//
+//  PUAbstractMeterContext.h
+//  PublicUtilities
+//
+//  Created by Philip Vasilchenko on 03.05.13.
+//  Copyright (c) 2013 CoreInvader. All rights reserved.
+//
+
+#import "AFNumberPickerExapmleViewController.h"
+
+
+@interface AFNumberPickerExapmleViewController ()
+
+@end
+
+
+@implementation AFNumberPickerExapmleViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.view.backgroundColor = [UIColor redColor];
+    }
+    return self;
+}
+
+@end
